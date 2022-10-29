60fc457609e9a0cd30ef1d1ad44981fe623d20f1
Guitared <guitared@users.noreply.github.com>
2022-10-29 13:08:02 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,37 +1,9 @@
-/*
- * This file is part of Connect4 Game Solver <http://connect4.gamesolver.org>
- * Copyright (C) 2017-2019 Pascal Pons <contact@gamesolver.org>
- *
- * Connect4 Game Solver is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * Connect4 Game Solver is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with Connect4 Game Solver. If not, see <http://www.gnu.org/licenses/>.
- */
 
 #include "Solver.hpp"
 #include <iostream>
 
 using namespace GameSolver::Connect4;
 
-/**
- * Main function.
- * Reads Connect 4 positions, line by line, from standard input
- * and writes one line per position to standard output containing:
- *  - score of the position
- *  - number of nodes explored
- *  - time spent in microsecond to solve the position.
- *
- *  Any invalid position (invalid sequence of move, or already won game)
- *  will generate an error message to standard error and an empty line to standard output.
- */
 int main(int argc, char** argv) {
   Solver solver;
   bool weak = false;
@@ -40,13 +12,9 @@ int main(int argc, char** argv) {
   std::string opening_book = "7x6.book";
   for(int i = 1; i < argc; i++) {
     if(argv[i][0] == '-') {
-      if(argv[i][1] == 'w') weak = true; // parameter -w: use weak solver
-      else if(argv[i][1] == 'b') { // paramater -b: define an alternative opening book
-        if(++i < argc) opening_book = std::string(argv[i]);
-      }
-      else if(argv[i][1] == 'a') { // paramater -a: make an analysis of all possible moves
-        analyze = true;
-      }
+      if(argv[i][1] == 'w') weak = true;
+      else if(argv[i][1] == 'b') {if(++i < argc) opening_book = std::string(argv[i]);}
+      else if(argv[i][1] == 'a') {analyze = true;}
     }
   }
   solver.loadBook(opening_book);
